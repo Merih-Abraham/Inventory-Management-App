caa06757387f928c093af1b036e42bc3bb60ea7d
Merih-Abraham <merih.abraham@dci-student.org>
2023-11-10 11:57:51 +0100

    This is my milestone8 commit

--- milestone_6/query.py
+++ milestone_6/query.py
@@ -1,258 +1,192 @@
-"""Command line interface to query the stock.
-To iterate the source data you can use the following structure:
-for item in warehouse1:
-    # Your instructions here.
-    # The `item` name will contain each of the strings (item names) in the list.
-"""
-
 from data import stock, personnel
-from datetime import datetime as dt
-from collections import Counter
+from datetime import datetime
+from collections import defaultdict
+
 
 def get_user_name():
-    name = input("Please provide your name: ")
-    return name
-
-def greet_user():
-    print("Hello",user_name)
-
-def display_menu(menu):
-    print ("Menu")
-    for k, v in menu.items(): 
-        print (f"{k}: {v}")
-
-def get_selected_operation():
-    selected_operation = input("Make your choice: ")
-    return selected_operation
-
-def count_of_warehouse():
-    list_of_warehouse = list({item["warehouse"] for item in stock})
-    return list_of_warehouse
-
-def list_items_by_warehouse():
-
-    count_of_warehouse = list({item["warehouse"] for item in stock})
-    count_of_items ={}
-
-    for i in range(1,(len(count_of_warehouse)+1)):
-        temp = 0
-        print(f"\nItems in Warehouse{count_of_warehouse[i-1]}\n")
-        for item in stock:
-            if item["warehouse"] == i:
-                print(f"{item['state']} {item['category']}")
-                temp += 1
-                count_of_items[i] = temp             
-
-    print("\n")
-
-    for k, v in count_of_items.items():
-        print(f"Total items in warehouse {k}: {v}")
-
-    return f"Listed {len(stock)} items."
+    username=input("Please enter the username: ")
+    return username.capitalize()
 
 
+def greet_user(username):
+    print(f"Hello {username}, Welcome to MARIOS BROS website")
+    
+    
 
-def get_item(input_item):
-    lst_item = input_item.split(" ")
-
-    if len(lst_item) == 3:
-        category = " ".join([lst_item[0], lst_item[1]])
-        state = lst_item[2]
-
-    elif len (lst_item) ==2:
-        category = lst_item[0]
-        state = lst_item[1]
-    else:
-        raise ValueError("Input is not valid. Try again")
-
-    return category.lower(), state.lower()
-
-def search_and_order_item():
-
-    try:
-
-        item_input = input("What is the name of the item? ")
-        selected_item =" ".join(get_item(item_input))
-        state, category = get_item(selected_item)
-
-        list_of_warehouse = count_of_warehouse()
-
-        info_of_item = []
-        count = {}
-
-        today = dt.now().date()
-        for i in range(1, (len(list_of_warehouse)+1)):
-            temp = 0
-            for item in stock:
-                if item['state'].lower() == state and item['category'].lower() ==category and item['warehouse'] == i:
-                    date_of_stock = dt.strptime(item["date_of_stock"].split()[0],'%Y-%m-%d').date()
-                    days_in_stock = (today - date_of_stock).days 
-                    info_of_item.append({f"- Warehouse {i}" : f"(in stock for {days_in_stock} days)"})
-                    temp += 1
-                    count[f'Warehouse {i}'] = temp
-
-
-        total = len(info_of_item)
-
-        print(f"Amount available: {total}")
-
-        print("Location:")
-
-        if total > 0:
-
-            for item in info_of_item:
-                for k,v in item.items():
-                    print(k, v)
-
-            maximum_in_warehouse = [(k, v) for k,v in count.items() if v == max(v for v in count.values())]
-
-            max_wrh, max_item = maximum_in_warehouse[0]
-
-            print (f"\nMaximum availability: {max_item} in {max_wrh}")
-
-            order = input("Would you like to order this item?(y/n): ").lower()
-            if order== "n":
-                return 
-
-            elif order == "y":
-
-                return order_item(selected_item, total)
-
-            else:
-                print( "Your answer has to be y or n")
-
-        else:
-            print("Not in stock")
-
-        return f"Search a {selected_item.capitalize()}"
-
-    except ValueError:
-        print('Input is not valid. Item includes more words then one.')
+def select_operation(): 
+    print("The following is the menu please choose the specific numeric associated with the choice. ")
+    print("1. List items by warehouse","2. Search an item and place an order","3. Browse by category","4. Quit", sep="\n")
+    print("-"*120)
+    menu_selection = input("Please type the number associated with the operation: ")
+    print()
+    # If user selects operation 1
+    if menu_selection == "1":
+        item_list_by_wearhouse()
+    # Else, if they pick 2
+    elif menu_selection=="2":
         search_and_order_item()
+        
+        # Else, if they pick 3
+    elif menu_selection == "3":
+        browse_by_category()
 
-def check_user_validation(func):
+    # Else, if they pick 4
+    elif menu_selection == "4":
+            pass
 
-    def wrapper(*args, **kwargs):
-        global user_name
-        global user_validation
-        if user_validation:
-            return func(*args, **kwargs)
+    else:
+        print("*"*50)
+        print("Invalid input, please enter a number between 1 and 4 for valid operation")
+        print("*"*50)
+   
+
+
+
+def item_list_by_wearhouse():
+    d=defaultdict(list)
+    for i in stock:
+        item_full_name=i["state"]+" "+i["category"]
+        d[i["warehouse"]].append(item_full_name)
+    for i in dict(d).keys():
+        print(f"Items in warehouse {i}: \n{dict(d)[i]} \n")
+        print(f"Total items in Warehouse {i} are {len(dict(d)[i])}")
+        print("."*120)
+    items_sum=0
+    for value in dict(d).values():
+        items_sum+=len(value)
+    number_of_warehouse=len(dict(d).keys())
+    actions.append(f"listed {items_sum} items from {number_of_warehouse} Warehouses")
+    # actions.append("Hi")
+    continue_session = input(f"Do you want to continue with another operation? (yes/no)")
+    if continue_session == "yes":
+        select_operation()
+
+                                
+def validate_user(func):
+    def wrapper(*args,**kwargs):
+        global username
+        global user_validate            
+        if user_validate:
+            return func(*args,**kwargs)
         else:
-            while not user_validation:
-                password = input("Please input your password: ")
-                for i in personnel:
-                    if user_name == i['user_name'] and password == i['password']:
-                        user_validation  = True
+            while not user_validate:
+                password=input(f"\nPlease enter the password {username} to grant access to place an order: ")
+                for i in personnel:   
+                    if username == i['user_name'] and password == i['password']:
+                        user_validate  = True
+                        print("-------- Access granted to place and order -------")
                         return func(*args, **kwargs)
                     for j in i.get ('head_of', []):
-                      if user_name == j['user_name'] and password == j['password']:
-                        user_validation  = True
+                      if username == j['user_name'] and password == j['password']:
+                        user_validate  = True
+                        print("-------- Access granted to place and order -------")
                         return func(*args, **kwargs)
-
-                answer = input ("User_name or password is not valid. Do you want to try again?y/n")
-                if answer =="y":
+                answer = input ("User_name or password is not valid. Do you want to try again?(yes/no) : ")
+                if answer =="yes":
                     user_name = get_user_name()
-                elif answer == "n":
-                    session_operation()
-
+                elif answer == "no":
+                    select_operation()    
     return wrapper
 
-@check_user_validation
-def order_item(selected_item, total):
-    while True:
-            quantity = int(input("How many would you like? "))
-            if quantity > total:
-                print(f"There are not this many available. The maximum amount that can be ordered is {total}")
-            elif quantity <= total:
-                print( f"{quantity} {selected_item.capitalize()} have been placed")
-                break
-    return f"Search and ordered the {selected_item.capitalize()}"  
+
+
+
+@validate_user
+def placing_order(search_item,total_item_count_in_Warehouses):
+    order_quantity = int(input(f"How much quantity of {search_item} do you want to order?"))
+    if order_quantity <= total_item_count_in_Warehouses:
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        print(f"Order placed: {order_quantity} * {search_item}")
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    else: 
+        print("-------------------------------------------------------------------------")
+        print(f"There are not this many available. The maximum quantity that can be ordered is {total_item_count_in_Warehouses}.")
+        print("--------------------------------------------------------------------------")
+        ask_order_max = input(f"Do you want to order the {search_item} in maximum quantity of {total_item_count_in_Warehouses}? (yes/no)")
+        if ask_order_max.lower() == "yes":
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            print(f"Order placed: {total_item_count_in_Warehouses} * {search_item}.")
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    
+
+def search_and_order_item():
+    search_item=input("Enter the item that you are searching:").lower()
+    search_item_state=" ".join(search_item.split(" ")[:-1]).capitalize()
+    search_item_category= search_item.split()[-1].capitalize()
+    location=[]
+    item_count_in_warehouse_dict={}
+    for item in stock:
+        if item["state"] == search_item_state and item["category"]==search_item_category:
+            date_str = item["date_of_stock"]
+            date_format = '%Y-%m-%d %H:%M:%S'
+            days=(datetime.now()-datetime.strptime(date_str, date_format)).days
+            location.append("Warehouse"+ str(item["warehouse"])+ f" (in stock for {days} days)") 
+            if item["warehouse"] in item_count_in_warehouse_dict:
+                item_count_in_warehouse_dict[item["warehouse"]]+=1
+            else:
+                item_count_in_warehouse_dict[item["warehouse"]]=1
+       
+    print(f"item_count_in_warehouse_dict:{item_count_in_warehouse_dict}")
+    if len(location)>0:
+        print(f"\nQuantity Availability: {len(location)}\n")
+        print("Location:")
+        for i in location:
+            print(i)
+        for warehouse,count in item_count_in_warehouse_dict.items():
+            if max(item_count_in_warehouse_dict.values())==count:                      
+                print(f"\nMaximum availability: {count} in Warehouse {warehouse}\n")
+        print("."*120)
+        place_order=input(f"Do you want to place an order for the item {search_item}? (yes/no)")
+        if place_order.lower()=="yes":
+            placing_order(search_item,sum(item_count_in_warehouse_dict.values()))
+    else:
+        print("Not in stock")
+
+    actions.append(f"Searched a {search_item}")
+    continue_session = input(f"\nDo you want to continue with another operation? (yes/no)")
+    if continue_session == "yes":
+        select_operation()
+
 
 def browse_by_category():
-
-    categories = []
+    # fix the code
+    list_item_category=[]
     for item in stock:
-        categories.append(item["category"])
+        list_item_category.append(item["category"])
+    dict_item_category_count={i:list_item_category.count(i) for i in list_item_category}
+    dict_id_category={}
+    for id,(key,value) in enumerate(dict_item_category_count.items()):
+        dict_id_category[id+1]=key
+        print(f"{id+1} {key} ({value})")
+    print()
+    select_category=input("Type the category number to browse:")
+    print()
+    category_name=None
+    for key_id, value_id in dict_id_category.items():
+        if key_id==int(select_category):
+            category_name=value_id
+            count_items_by_category=0
+            for item in stock:
+                if value_id==item["category"]:
+                    count_items_by_category+=1
+                    print(f"{item['state']} {item['category']}, Warehouse {item['warehouse']}")
+    print("."*120)
+    print(f"Total items in this category are: {count_items_by_category}")
+    print("."*120)
+    actions.append(f"Browsed the category {category_name}")
+    continue_session = input(f"\nDo you want to continue with another operation? (yes/no)")
+    if continue_session == "yes":
+        select_operation()
 
-    count = Counter(categories)
-
-    summary_of_categories = []
-
-    i = 1
-    for k, v in count.items():
-        summary_of_categories.append((i, k, v))
-        i+=1
-
-    for i in summary_of_categories:
-        print(f"{i[0]}. {i[1]} ({i[2]})")
-
-
-    number_of_category = int(input("Type the number of the category to browse: "))
-
-    selected_category = None
-
-    for category_info in summary_of_categories:
-        if number_of_category == category_info[0]:
-            selected_category = category_info[1]
-            break  
-
-    print(f"\nList of {selected_category} available:")
-
-    for i in stock:
-        if i['category'] == selected_category:
-            match_state = i["state"]
-            match_warehouse = i["warehouse"]
-            print(f"{match_state} {selected_category.lower()}, Warehouse {match_warehouse}")
-        else: 
-            ("Input is not valid")
-            session_operation()
-
-    return f"Browsed the category {selected_category}"                     
-
-session_actions = []
-
-def session_operation():
-
-    menu = {"1": "List items by warehouse", "2": "Search an item and place an order","3": "Browse by category", "4": "Quit" }
-    display_menu(menu)
-    operation = get_selected_operation()
-
-    if operation == 4:
-        pass
-
-    elif operation != 4 :
-        if operation == "1":
-                action = list_items_by_warehouse()
-                session_actions.append(action)        
-        elif operation == "2":
-                action = search_and_order_item()
-                session_actions.append(action)         
-        elif operation == "3":
-            action = browse_by_category()
-            session_actions.append(action)
-
-        else:
-            print("*" * 50)
-            print(operation, "is not a valid operation.")
-            print("*" * 50)
-            session_operation()
-
-        working_continue = input("Would you like to continue to work with program?(y/n) ").lower()
-
-        if working_continue == "y":
-            session_operation()
-        elif working_continue != "n":
-            print ("Your answer has to be y or n")
-
-user_name = get_user_name()
-greet_user()
-user_validation = False
-session_operation()
-print(f"Thank you for your visit, {user_name}!")
-
-if session_actions:
-        print("In this session you have:")
-        for i, action in enumerate(session_actions, start=1):
-            print(f"""    {i}. {action}""")
-
-
+actions=[]
+username=get_user_name()
+user_validate=False
+print("*"*120)
+greet_user(username)
+print("."*120)
+menu_selection= select_operation()
+print()
+print(f"Thank you for your visit, {username}")
+print(f"In this session you have:")
+for id, stmt in enumerate(actions):
+    print(id+1,".",stmt)
